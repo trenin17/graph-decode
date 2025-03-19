b5da52dd17a8f21f1397203b0c6938cefe079140
Aleksandr Trenin <aleksandr.trenin@intel.com>
2025-03-19 13:40:43 +0000

    more iteration for release build

--- src/main.cpp
+++ src/main.cpp
@@ -13,7 +13,7 @@ int main() {
     engine.run();
 
     std::cout << "Pushing input" << std::endl;
-    for (int i = 0; i < 1000000; i++) {
+    for (int i = 0; i < 10000000; i++) {
         engine.pushInput({i});
     }
     engine.pushInput({-1, true});
